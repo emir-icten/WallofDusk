f7a78ef11c3f8e5d022e26be609c108b5f865351
emir-icten <emirikler55@gmail.com>
2025-12-02 04:03:40 +0300

    emir

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -2,34 +2,52 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    // === PARA BÝRÝMLERÝ (Yaratýklardan ve Meta Geliþimden Gelir) ===
-    public static int Coins = 0;      // Düþmanlardan kazanýlan para (In-game Upgrade)
-    public static int Crystals = 0;   // Kalýcý geliþim parasý (Meta Progression)
+    public static ResourceManager instance;
 
-    // === ENVANTER (HAMMADDE - Sadece Kaynak Toplamadan Gelir) ===
-    public static int WoodCount = 0;  // Odun Malzemesi
-    public static int StoneCount = 0; // Taþ Malzemesi
+    // === PARA BÝRÝMLERÝ ===
+    public static int Coins = 0;
+    public static int Crystals = 0;
+
+    // === ENVANTER (HAMMADDE) ===
+    public static int WoodCount = 0;
+    public static int StoneCount = 0;
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+    }
 
     // YENÝ EKLEME FONKSÝYONLARI:
     public static void AddWood(int amount)
     {
         WoodCount += amount;
         Debug.Log("Odun eklendi! Toplam: " + WoodCount);
+
+        // Ekrana Haber Veriyoruz (KÖPRÜ)
+        if (UIManager.instance != null)
+            UIManager.instance.UpdateWoodUI(WoodCount);
     }
 
     public static void AddStone(int amount)
     {
         StoneCount += amount;
         Debug.Log("Taþ eklendi! Toplam: " + StoneCount);
+
+        // Ekrana Haber Veriyoruz (KÖPRÜ)
+        if (UIManager.instance != null)
+            UIManager.instance.UpdateStoneUI(StoneCount);
     }
 
     public static void AddCoin(int amount)
     {
         Coins += amount;
         Debug.Log("Coin Eklendi. Toplam: " + Coins);
+
+        // Ekrana Haber Veriyoruz (KÖPRÜ)
+        if (UIManager.instance != null)
+            UIManager.instance.UpdateCoinUI(Coins);
     }
 
-    // Kristal ekleme fonksiyonunu silmedik, ileride kullanacaðýz
     public static void AddCrystals(int amount)
     {
         Crystals += amount;
